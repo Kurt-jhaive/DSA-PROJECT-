233086d9a88515ba6991ca05ab8b2725973bb337
Kurt Jhaive <saldikurtjhaive@gmail.com>
2023-12-15 00:32:55 +0800

    added terms and condition forms (tom na ulit ako mag code)

---
+++ termsframe2.py
@@ -0,0 +1,94 @@
+from tkinter import *
+from tkinter import messagebox
+import subprocess
+
+
+from pathlib import Path
+OUTPUT_PATH = Path(__file__).parent
+ASSETS_PATH = OUTPUT_PATH / Path(r"forms\termsform2_resources")
+
+
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
+def back_button_clicked():
+    window.destroy()
+    subprocess.Popen(["python", "termsframe1.py"])
+def close_window():
+    if messagebox.askokcancel("Exit", "Do you really want to exit?"):
+        window.destroy()
+
+window = Tk()
+
+# Get the screen width and height
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+# Set the protocol for the window close event
+window.protocol("WM_DELETE_WINDOW", close_window)
+
+# Calculate the x and y coordinates for the window to be centered
+x = (screen_width - 820) // 2
+y = (screen_height - 500) // 2
+
+window.geometry(f"820x500+{x}+{y}")
+window.configure(bg="#FFFFFF")
+canvas = Canvas(
+    window,
+    bg = "#FFFFFF",
+    height = 500,
+    width = 820,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge"
+)
+
+canvas.place(x = 0, y = 0)
+button_image_1 = PhotoImage(
+    file=relative_to_assets("button_1.png"))
+back_button = Button(
+    image=button_image_1,
+    borderwidth=0,
+    highlightthickness=0,
+    command=back_button_clicked,
+    relief="flat"
+)
+back_button.place(
+    x=537.0,
+    y=421.0,
+    width=112.89242553710938,
+    height=39.0
+)
+
+button_image_2 = PhotoImage(
+    file=relative_to_assets("button_2.png"))
+agree_button = Button(
+    image=button_image_2,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("agree_button clicked"),
+    relief="flat"
+)
+agree_button.place(
+    x=659.0,
+    y=421.0,
+    width=112.89242553710938,
+    height=39.0
+)
+
+image_image_1 = PhotoImage(
+    file=relative_to_assets("image_1.png"))
+image_1 = canvas.create_image(
+    409.0,
+    113.0,
+    image=image_image_1
+)
+
+image_image_2 = PhotoImage(
+    file=relative_to_assets("image_2.png"))
+image_2 = canvas.create_image(
+    408.0,
+    304.0,
+    image=image_image_2
+)
+window.resizable(False, False)
+window.mainloop()
